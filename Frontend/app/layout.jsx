7631b17b6d245f7517be9d34fd1d74f4054df7db
Lega-s <levy.schaerer@icloud.com>
2025-07-28 12:40:09 +0200

    Initialized Project

---
+++ Frontend/app/layout.jsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+export const metadata = {
+	title: "Bankraum",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
+}
