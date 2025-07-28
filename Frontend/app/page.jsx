6799151c4b5e5c105c016ab88c39cb3f88649d0c
Lega-s <levy.schaerer@icloud.com>
2025-07-28 13:23:29 +0200

    Header Created and styled

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<div>
-			<img src="/Image1.JPG" alt="sry" height={1280} width={1920} />
+			<img src="/Image1.JPG" alt="sry" height={1980} width={1920} />
 		</div>
 	);
 }
--- Frontend/components/Header/Header.jsx
+++ Frontend/components/Header/Header.jsx
@@ -1,7 +1,16 @@
+'use client'
+import styles from './Header.module.css'
+
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className={styles.body}>
+            <h1 onClick={() => window.location.href = '/'}>Bankraum</h1>
+            <div className={styles.navigation}>
+                <a href='/portfolio'>Portfolio</a>
+                <a href='/blog'>Blog</a>
+                <a href='/ueber'>Über mich</a>
+                <a href='/kontakt'>Kontakt</a>
+            </div>
         </div>
     )
 }
